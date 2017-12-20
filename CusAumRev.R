8b3f6b41cc39707298432dbf59a2f16c4074fbb0
Karen <smileleo0803@gmail.com>
2017-12-20 21:25:10 +0800

    Delete CusAumRev.R

--- CusAumRev.R
+++
@@ -1,4 +0,0 @@
-#合併AUM及REV
-f <- merge(x = AUM, y = REV, by.x=c("IDNO_AUM","CUSTNO_AUM"),by.y=c("IDNO_REV","CUSTNO_REV"), all = TRUE)
-library(dplyr)
-f <- full_join(x = AUM, y = REV,by = "CUSTNO_")
